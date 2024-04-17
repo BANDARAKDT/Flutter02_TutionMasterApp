21c8cac14946808748a7506da289ebfc5b819375
BANDARAKDT <deshanibandarawas@gmail.com>
2024-04-17 23:15:25 +0530

    dashboard of app

---
+++ flutter_application_1/lib/Screens/Dashboard.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class _DashboardState extends StatefulWidget {
+  const _DashboardState({super.key});
+
+  @override
+  State<_DashboardState> createState() => __DashboardStateState();
+}
+
+class __DashboardStateState extends State<_DashboardState> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
