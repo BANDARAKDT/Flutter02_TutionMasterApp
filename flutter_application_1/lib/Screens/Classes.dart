7eddcdd478b7dd867c4a8d70e2ae0b029eed7845
BANDARAKDT <deshanibandarawas@gmail.com>
2024-04-17 23:17:28 +0530

    create class screen

---
+++ flutter_application_1/lib/Screens/Classes.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class _ClasseState extends StatefulWidget {
+  const _ClasseState({super.key});
+
+  @override
+  State<_ClasseState> createState() => __ClasseStateState();
+}
+
+class __ClasseStateState extends State<_ClasseState> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
