70001b12684c8131f05cb99f3d1bf4a836e8cf95
BANDARAKDT <deshanibandarawas@gmail.com>
2024-04-19 23:09:18 +0530

    attendence

--- flutter_application_1/lib/Screens/Attendence.dart
+++ flutter_application_1/lib/Screens/Attendence.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 
-
 class Attendence extends StatefulWidget {
-  const Attendence({super.key});
+  const Attendence({Key? key}) : super(key: key);
 
   @override
   State<Attendence> createState() => _AttendenceState();
@@ -11,11 +10,33 @@ class Attendence extends StatefulWidget {
 class _AttendenceState extends State<Attendence> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "Attendence Screen",
-      theme: ThemeData(
-        primaryColor: Colors.purple[400],
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Attendance Screen'),
       ),
+      body: Center(
+        child: Text(
+          'Attendance Content',
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+    );
+  }
+}
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Attendence(),
     );
   }
 }
