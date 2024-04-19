4a61c0714d59b1fbdb7f5d38e8b48a4b4164229e
BANDARAKDT <deshanibandarawas@gmail.com>
2024-04-19 23:12:33 +0530

    help screen

--- flutter_application_1/lib/Screens/Help.dart
+++ flutter_application_1/lib/Screens/Help.dart
@@ -1,15 +1,42 @@
 import 'package:flutter/material.dart';
 
 class Help extends StatefulWidget {
-  const Help({super.key});
+  const Help({Key? key}) : super(key: key);
 
   @override
-  State<Help> createState() => _HelpState();
+  _HelpState createState() => _HelpState();
 }
 
 class _HelpState extends State<Help> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Help Screen'),
+      ),
+      body: Center(
+        child: Text(
+          'Help Content',
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+    );
+  }
+}
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Help(),
+    );
   }
 }
